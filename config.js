65894c93261f36e97305197d09d208007a75a39e
zhechangou-hue <zhechangou@gmail.com>
2026-06-09 14:29:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  trackingEnabled: false,
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  trackingEnabled: true,
+  supabaseUrl: "https://rscxrqrwlhgxafgrcqkd.supabase.co",
+  supabaseAnonKey: "sb_publishable_hm3dHtqmdJvcWt6O1EC8cQ_o8bxKfDf"
 };
